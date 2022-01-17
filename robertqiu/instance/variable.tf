4599e2138bbd12b0a32dbdc32d740f7bae6cb978
robertqiu <robertqiu@tencent.com>
2022-01-17 17:18:45 +0800

    update

---
+++ robertqiu/instance/variable.tf
@@ -0,0 +1,20 @@
+/* This is used to configure the variable */
+variable "TENCENTCLOUD_REGION" {
+  description = "This is region."
+  type = string
+  default = "ap-guangzhou"
+}
+
+variable "TENCENTCLOUD_SECRET_ID" {
+  description = "This is secret_id."
+  type = string
+  /* Set your Tencent Secret ID here prior to run the terraform command */
+  default = "****************"
+}
+
+variable "TENCENTCLOUD_SECRET_KEY" {
+  description = "This is secret key."
+  type = string
+  /* Set your Tencent Cloud Secret Key here prior to run the terraform command */
+  default = "****************"
+}
