b97bec7b3606b35041d13c662c563cf28942dafb
robertqiu <robertqiu@tencent.com>
2022-01-18 09:55:57 +0800

    update

---
+++ robertqiu/antiDDoS-L4-Rule/data.tf
@@ -0,0 +1,11 @@
+data "tencentcloud_dayu_l4_rules" "name_test" {
+  resource_type = tencentcloud_dayu_l4_rule.test_rule.resource_type
+  resource_id   = tencentcloud_dayu_l4_rule.test_rule.resource_id
+  name          = tencentcloud_dayu_l4_rule.test_rule.name
+}
+
+data "tencentcloud_dayu_l4_rules" "id_test" {
+  resource_type = tencentcloud_dayu_l4_rule.test_rule.resource_type
+  resource_id   = tencentcloud_dayu_l4_rule.test_rule.resource_id
+  rule_id       = tencentcloud_dayu_l4_rule.test_rule.rule_id
+}
